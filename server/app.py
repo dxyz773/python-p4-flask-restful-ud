f38bf7b14466756830e8f029bf7184a8b5b9d9d3
demitryedwards <dxyz773@gmail.com>
2023-06-17 23:52:36 -0400

    complete

--- server/app.py
+++ server/app.py
@@ -7,8 +7,8 @@ from flask_restful import Api, Resource
 from models import db, Newsletter
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newsletters.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///newsletters.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
@@ -16,14 +16,13 @@ db.init_app(app)
 
 api = Api(app)
 
-class Home(Resource):
 
+class Home(Resource):
     def get(self):
-        
         response_dict = {
             "message": "Welcome to the Newsletter RESTful API",
         }
-        
+
         response = make_response(
             response_dict,
             200,
@@ -31,12 +30,12 @@ class Home(Resource):
 
         return response
 
-api.add_resource(Home, '/')
+
+api.add_resource(Home, "/")
+
 
 class Newsletters(Resource):
-
     def get(self):
-        
         response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
 
         response = make_response(
@@ -47,10 +46,9 @@ class Newsletters(Resource):
         return response
 
     def post(self):
-        
         new_record = Newsletter(
-            title=request.form['title'],
-            body=request.form['body'],
+            title=request.form["title"],
+            body=request.form["body"],
         )
 
         db.session.add(new_record)
@@ -65,12 +63,12 @@ class Newsletters(Resource):
 
         return response
 
-api.add_resource(Newsletters, '/newsletters')
+
+api.add_resource(Newsletters, "/newsletters")
+
 
 class NewsletterByID(Resource):
-
     def get(self, id):
-
         response_dict = Newsletter.query.filter_by(id=id).first().to_dict()
 
         response = make_response(
@@ -80,8 +78,29 @@ class NewsletterByID(Resource):
 
         return response
 
-api.add_resource(NewsletterByID, '/newsletters/<int:id>')
+    def patch(self, id):
+        data = request.get_json()
+        updated_newsletter = Newsletter.query.filter_by(id=id).first()
+        for attr in data:
+            setattr(updated_newsletter, attr, data.get(attr))
+        db.session.add(updated_newsletter)
+        db.session.commit()
+
+        updated_dict = updated_newsletter.to_dict()
+        response = make_response(updated_dict, 200)
+        return response
+
+    def delete(self, id):
+        selected_newsletter = Newsletter.query.filter_by(id=id).first()
+        db.session.delete(selected_newsletter)
+        db.session.commit()
+
+        response = make_response({}, 200)
+        return response
 
 
-if __name__ == '__main__':
+api.add_resource(NewsletterByID, "/newsletters/<int:id>")
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
